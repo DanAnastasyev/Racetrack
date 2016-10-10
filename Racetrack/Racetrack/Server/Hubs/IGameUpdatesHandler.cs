121813d363b395573eafc0946133de1e64ddf8b8
DanAnastasyev <dan.anastasyev@gmail.com>
2016-10-11 00:34:20 +0300

    [Server] Обновление сервера

--- Racetrack/Racetrack/Server/Hubs/IGameUpdatesHandler.cs
+++ Racetrack/Racetrack/Server/Hubs/IGameUpdatesHandler.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Racetrack.Server.Hubs {
+﻿namespace Racetrack.Server.Hubs {
 	// Коллбеки в GameHub для обновления состояния игры
 	public interface IGameUpdatesHandler {
 		void CrashCar(string playerId);
