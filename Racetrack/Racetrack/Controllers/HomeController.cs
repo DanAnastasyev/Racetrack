1e126ded64a163cddbca736196aadea4fe6dfb44
DanAnastasyev <dan.anastasyev@gmail.com>
2016-10-30 21:00:49 +0300

    [Server] Добавил рейтинг на Entity Framework

--- Racetrack/Racetrack/Controllers/HomeController.cs
+++ Racetrack/Racetrack/Controllers/HomeController.cs
@@ -1,17 +1,56 @@
-﻿using System.Collections.Generic;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity.Owin;
-using Racetrack.Models;
+using PagedList;
+using Racetrack.Context;
 
 namespace Racetrack.Controllers {
 	public class HomeController : Controller {
+		private readonly ResultContext _db = new ResultContext();
+
 		public ActionResult Index() {
 			return View();
 		}
 
 		public ActionResult About() {
 			return View();
+		}
+
+		public ActionResult Rating(string sortOrder, string currentFilter, string searchString, int? page) {
+			ViewBag.CurrentSort = sortOrder;
+			ViewBag.MovementsSortParam = string.IsNullOrEmpty(sortOrder) ? "movement_desc" : "";
+			ViewBag.NameSortParam = sortOrder == "Name" ? "name_desc" : "Name";
+
+			if (searchString != null) {
+				page = 1;
+			} else {
+				searchString = currentFilter;
+			}
+
+			ViewBag.CurrentFilter = searchString;
+			
+			var players = from s in _db.Results
+						  select s;
+			if (!string.IsNullOrEmpty(searchString)) {
+				players = players.Where(s => s.PlayerName.StartsWith(searchString));
+			}
+			switch (sortOrder) {
+				case "movement_desc":
+					players = players.OrderByDescending(s => s.NumberOfMovements);
+					break;
+				case "Name":
+					players = players.OrderBy(s => s.PlayerName);
+					break;
+				case "name_desc":
+					players = players.OrderByDescending(s => s.PlayerName);
+					break;
+				default:
+					players = players.OrderBy(s => s.NumberOfMovements);
+					break;
+			}
+
+			int pageSize = 3;
+			int pageNumber = (page ?? 1);
+			return View(players.ToPagedList(pageNumber, pageSize));
 		}
 	}
 }
---
+++ Racetrack/Racetrack/Models/Result.cs
@@ -0,0 +1,8 @@
+﻿namespace Racetrack.Models {
+	public class Result {
+		public int Id { get; set; }
+		public string PlayerName { get; set; }
+		public int NumberOfMovements { get; set; }
+		//public GameLogModel GameLog { get; set; }
+	}
+}
