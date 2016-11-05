421f0d1c292091889157107d319918cb6f7089b8
DanAnastasyev <dan.anastasyev@gmail.com>
2016-11-06 00:04:43 +0300

    [Server][AI] Подсчет мин. расстояния до финиша из каждой клетки поля

--- Racetrack/Racetrack/GameServer/AI/Strategy.cs
+++ Racetrack/Racetrack/GameServer/AI/Strategy.cs
@@ -8,6 +8,7 @@ namespace Racetrack.GameServer.AI {
 	public class Strategy {
 		public readonly WorldModel WorldModel;
 		private List<List<int>> distancesToFinish = new List<List<int>>();
+		private static readonly int MaxPathLength = int.MaxValue;
 
 		private void AddFinishLine(Queue<Coordinates> bfsQueue) {
 			Line finishLine = WorldModel.GetFinishLine();
@@ -51,8 +52,40 @@ namespace Racetrack.GameServer.AI {
 
 			var bfsQueue = new Queue<Coordinates>();
 			AddFinishLine(bfsQueue);
+			var finishLineNumber = WorldModel.WayPointsCount() - 1;
 
+			while (bfsQueue.Count != 0) {
+				var curNode = bfsQueue.Dequeue();
 
+				if (distancesToFinish[curNode.Y][curNode.X] != -1) {
+					continue;
+				}
+				// Ищем минимальное значение расстояния до финиша среди соседей
+				int minDistance = MaxPathLength; 
+				for (int i = -1; i <= 1; ++i) {
+					for (int j = -1; j <= 1; ++j) {
+						if (!WorldModel.IsEmptyPosition(curNode.X + j, curNode.Y + i)) {
+							continue;
+						}
+						// Если на клетку можно встать, её еще не посетили 
+						//  и она не находится с другой стороны от финиша - идем в неё
+						var movementLine = new Line(new Coordinates(curNode.Y, curNode.X),
+							new Coordinates(curNode.Y + j, curNode.X + i));
+						var reverseMovementLine = new Line(new Coordinates(curNode.Y + j, curNode.X + i),
+							new Coordinates(curNode.Y, curNode.X));
+						if (!WorldModel.FindIntersectedWayPoints(movementLine).Contains(finishLineNumber)
+						    && distancesToFinish[curNode.Y + j][curNode.X + i] == -1) {
+							bfsQueue.Enqueue(new Coordinates(curNode.X + j, curNode.Y + i));
+						} else if (!WorldModel.FindIntersectedWayPoints(reverseMovementLine).Contains(finishLineNumber)
+						           && distancesToFinish[curNode.Y + j][curNode.X + i] != -1) {
+							// Уже посещенная вершина
+							minDistance = Math.Min(distancesToFinish[curNode.Y + j][curNode.X + i], minDistance);
+						}
+					}
+				}
+				// Мин. расстояние до текущей вершины выражается через мин. расстояние до соседей
+				distancesToFinish[curNode.Y][curNode.X] = minDistance + 1;
+			}
 		}
 
 //		public MoveModel NextMove(List<PlayerModel> players, int playerNumber) {
